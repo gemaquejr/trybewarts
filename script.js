8d554aec69dd4138b968e406f9ec9ecef54665cb
Muller Borges <msbhero@gmail.com>
2021-09-23 19:30:07 -0300

    18. Faça com que o botão Enviar seja habilitado somente após a checkbox do requisito 16 ser selecionada

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const email = document.querySelector('#email');
 const pass = document.querySelector('#password');
 const btnLogin = document.querySelector('#login');
+const agreed = document.querySelector('#agreement');
+const btnSubmit = document.querySelector('#submit-btn');
 
 btnLogin.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && pass.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+agreed.addEventListener('click', (event) => {
+  event.preventDefault();
+  if (agreed.checked) {
+    btnSubmit.removeAttribute('disabled');
   }
 });
