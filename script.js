e202a2a7170edec325f1c526c035aa980a6393ac
Muller Borges <msbhero@gmail.com>
2021-09-28 23:21:47 -0300

    requisito 21 parcial

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const agreed = document.querySelector('#agreement');
 const btnSubmit = document.querySelector('#submit-btn');
 const textArea = document.querySelector('textarea');
 const count = document.querySelector('#counter');
+const inputName = document.querySelector('#input-name');
+const inputLastName = document.querySelector('#input-lastname');
+const form = document.querySelector('#evaluation-form');
+const inputEmail = document.querySelector('#input-email');
+const inputCasa = document.querySelector('#house');
 
 textArea.addEventListener('keyup', () => {
   const limit = 500 - textArea.value.length;
@@ -26,7 +31,20 @@ agreed.addEventListener('click', (event) => {
   }
 });
 
-function sendForm() {
-  agreed.innerHTML = '';
-  agreed.innerHTML += `<p>Nome: ${name.value} </p>`
-}
+btnSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const name = document.createElement('span');
+  const emailB = document.createElement('span');
+  const casa = document.createElement('span');
+  const house = inputCasa.options[inputCasa.selectedIndex].text;
+  const femSel = document.querySelector('input[name="family"]:checked').value;
+  const materia = document.querySelectorAll('input[class="subject"]:checked').value;
+  name.innerText = `Nome: ${inputName.value} ${inputLastName.value}`;
+  emailB.innerText = `Email: ${inputEmail.value}`;
+  casa.innerText = `Casa: ${house}`;
+  form.innerHTML = `${name.innerText} 
+  ${emailB.innerText} 
+  ${casa.innerText} 
+  Família: ${femSel}
+  Matérias: ${materia}`;
+});
